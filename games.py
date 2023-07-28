0bede29891de105d8bea093503b852060023f16e
namita1005s <137642335+namita1005s@users.noreply.github.com>
2023-07-28 12:19:42 -0700

    game

--- games.py
+++ games.py
@@ -1,70 +1,62 @@
 import pgzrun
 from random import randint
-
-#screen size
+# screen size
 WIDTH = 1000
 HEIGHT = 500
-
-#objects
-p = Actor('hero') #Actor is a class
+# objects
+p = Actor('hero')
 e = Actor('enemy')
 c = Actor('friut')
-
-#configuration
+# configs
 c.x = randint(50, WIDTH-50)
 c.y = randint(50, HEIGHT-50)
-p.pos = (WIDTH/2, HEIGHT/2)
+p.pos = (WIDTH/2, HEIGHT/2)  
 e.pos = (-100, HEIGHT/2)
-
 ps = 5
 es = 2
 score = 0
 def player_control():
+    # player control
     if keyboard.left:
         p.x -= ps
     if keyboard.right:
         p.x += ps
     if keyboard.up:
-        p.y += ps
-    if keyboard.down:
         p.y -= ps
+    if keyboard.down:
+        p.y += ps
     if keyboard.space:
-        p.angle += ps
-
+        p.angle += ps 
 def enemy_tracking():
-    #enemy control
+    # enemy tracks player
     if p.x > e.x:
         e.x += es
     if p.x < e.x:
-        e.x -= es
+        e.x -= es 
     if p.y > e.y:
-        p.y += es
+        e.y += es
     if p.y < e.y:
         e.y -= es
     print(f'player {p.pos} enemy {e.pos}')
     if e.colliderect(p):
         exit()
-
 def fruit_eating():
+    # fruit collision
     global score
-    #fruit collision
     if p.colliderect(c):
-        c.x= randint(50, WIDTH-50)
-        c.y= randint(50, HEIGHT-50)
+        c.x = randint(50, WIDTH-50)
+        c.y = randint(50, HEIGHT-50)
         score += 10
         sounds.clap.play()
-
-#drawing on screen
-def draw(): #class actor behaviour
+def draw():
     screen.clear()
     p.draw()
     e.draw()
     c.draw()
-    screen.draw.text(f'Score: {score}',(10,10), color='white')
-def update(dt): #Control the player
+    screen.draw.text(f'Score: {score}', (10, 10), color='white')
+def update(dt):
     player_control()
     enemy_tracking()
     fruit_eating()
-
-#game loop
+# game loop
 pgzrun.go()
